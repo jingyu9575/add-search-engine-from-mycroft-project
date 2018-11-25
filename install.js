8845bf55befd120fd500a2636e61214b9e1f98ea
jingyu9575 <qw2g64@zoho.eu>
2018-11-25 20:26:16 +0800

    v0.4: lower strict_min_version to 60 and add instructions

--- install.js
+++ install.js
@@ -4,7 +4,12 @@ for (const element of document.querySelectorAll('[data-i18n]'))
 const link = document.createElement('link')
 link.setAttribute('rel', 'search')
 link.setAttribute('type', 'application/opensearchdescription+xml')
-link.setAttribute('title', 'Install Search Engine ' + Date.now() +
+link.setAttribute('title', browser.i18n.getMessage('searchEngine') + ' ' + Date.now() +
 	Math.floor(Math.random() * 1000000))
 link.setAttribute('href', decodeURIComponent(location.hash.replace(/^#/, '')))
 document.head.appendChild(link)
+
+browser.runtime.getBrowserInfo().then(info => {
+	if (Number(info.version.replace(/\D.*/, '')) <= 60)
+		document.documentElement.classList.add('firefox60')
+})
